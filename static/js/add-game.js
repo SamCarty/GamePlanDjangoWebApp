6b1a26128cfa56b230712afb578be2bc0500d079
Sam Carty <samcarty311@gmail.com>
2020-01-12 21:57:02 +0000

    modularized js

--- static/js/add-game.js
+++ static/js/add-game.js
@@ -1,3 +1,20 @@
+function getContentRecommendations(gameName, element) {
+    url = '/recommender/content-based/' + gameName + '/' + 60;
+    fetchRecommendations(url, element);
+}
+
+function fetchRecommendations(url, section) {
+    $.getJSON(url, function (result) {
+        if (result.data != null) {
+            section.style.display = 'block';
+            Object.values(result.data).forEach(function (key) {
+                addRecommendation(key, section);
+            });
+            createSlider(section);
+        }
+    });
+}
+
 function addRecommendation(game, section) {
     itemDiv = document.createElement('div');
     itemDiv.setAttribute('class', 'slider-cell');
@@ -17,4 +34,16 @@ function addRecommendation(game, section) {
     itemLink.appendChild(itemTitle);
     itemDiv.appendChild(itemLink);
     section.appendChild(itemDiv);
+}
+
+function createSlider(section) {
+    new Flickity(section, {
+        cellAlign: 'left',
+        initialIndex: 0,
+        lazyLoad: 9,
+        prevNextButtons: true,
+        pageDots: false,
+        setGallerySize: true,
+        wrapAround: true
+    });
 }
