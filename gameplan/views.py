331d1d34538527fa8c54ebd85ab83a7a0bad9969
Sam Carty <samcarty311@gmail.com>
2020-01-24 16:41:23 +0000

    added pagination on search

--- gameplan/views.py
+++ gameplan/views.py
@@ -1,9 +1,9 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
 from django.views.generic import ListView
 from datetime import datetime
-
 
 from recommender.views import get_top_charts_recommendations
 from gameplan.models import Game, Genre, Platform
@@ -44,18 +44,62 @@ def search(request, title):
 
 
 class SearchResultsView(ListView):
+    paginate_by = 6
     model = Game
     template_name = 'gameplan/search.html'
 
-    def get_queryset(self):
-        query = self.request.GET.get('q')
+    def get(self, request, *args, **kwargs):
+        context = dict()
+        context['session_id'] = create_session(request)
+        context['genres'] = get_all_genres()
 
+        platforms = list()
+        genres = list()
+
+        query = self.request.GET.get('q')
         games = Game.objects.filter(title__icontains=query)
+        if self.request.GET.get('p') is not None:
+            q_platforms = self.request.GET.get('p')
+            q_platforms_list = [int(x) for x in q_platforms.split(',')]
+            games = games.filter(platforms__platform_id__in=q_platforms_list)
+        if self.request.GET.get('g') is not None:
+            q_genres = self.request.GET.get('g')
+            q_genres_list = [int(x) for x in q_genres.split(',')]
+            games = games.filter(genres__genre_id__in=q_genres_list)
+
         for game in games:
             frd = int(game.first_release_date)
             game.first_release_date = datetime.utcfromtimestamp(frd).strftime('%d/%m/%Y')
 
-            game.platforms_human = list(Platform.objects.filter(
-                platform_id__in=game.platforms.all().values_list('platform_id', flat=True)).values('name'))
+            platforms_full = list(Platform.objects.filter(
+                platform_id__in=game.platforms.all().values_list('platform_id', flat=True)).values('name',
+                                                                                                   'platform_id'))
 
-        return games
+            game.platforms_full = platforms_full
+            for platform in platforms_full:
+                if platform not in platforms:
+                    platforms.append(platform)
+
+            genres_full = list(Genre.objects.filter(
+                genre_id__in=game.genres.all().values_list('genre_id', flat=True)).values('name', 'genre_id'))
+
+            game.genres_full = genres_full
+            for genre in genres_full:
+                if genre not in genres:
+                    genres.append(genre)
+
+        context['search_results_platforms'] = platforms
+        context['search_results_genres'] = genres
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(games, 12)
+        try:
+            games_page = paginator.page(page)
+        except PageNotAnInteger:
+            games_page = paginator.page(1)
+        except EmptyPage:
+            games_page = paginator.page(paginator.num_pages)
+
+        context['search_results'] = games_page
+
+        return render(request, self.template_name, context)
