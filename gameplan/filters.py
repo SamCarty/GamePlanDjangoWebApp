cacf5d7b3fb93f137b11171c3b13004361ec2b99
Sam Carty <samcarty311@gmail.com>
2020-01-28 11:10:43 +0000

    add platform and genre links from details page

--- gameplan/filters.py
+++ gameplan/filters.py
@@ -7,9 +7,9 @@ from gameplan.models import Game, Platform, Genre
 class GameFilter(df.FilterSet):
     q = df.CharFilter(label='Query', field_name='title', lookup_expr='icontains')
     platforms = df.ModelMultipleChoiceFilter(label='Platform', queryset=Platform.objects.all().order_by('name'),
-                                             widget=forms.CheckboxSelectMultiple)
+                                             widget=forms.CheckboxSelectMultiple, conjoined=True)
     genres = df.ModelMultipleChoiceFilter(label='Genre', queryset=Genre.objects.all().order_by('name'),
-                                          widget=forms.CheckboxSelectMultiple)
+                                          widget=forms.CheckboxSelectMultiple, conjoined=True)
 
     class Meta:
         model = Game
